96a99f3245213cee1dcbf49b65985822d31fa3ec
Usama Bhakrani <usamabhakrani@gmail.com>
2024-03-31 22:35:57 +0500

    settingup router

--- app/index.js
+++ app/index.js
@@ -1,12 +1,21 @@
 import { View, Text, ScrollView, SafeAreaView } from "react-native";
 import React from "react";
 import { Stack, useRouter } from "expo-router";
+import { images, icons, COLORS, FONT, SIZES, SHADOWS } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
 
 const Home = () => {
+  const router = useRouter();
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView>
+      <Stack.Screen/>
+    </SafeAreaView>
   );
 };
 
